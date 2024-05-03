acce78a35e4b3211d7adbfd79c7f384aa61ae3f8
Rohith Krishna <roal2027@gmail.com>
2024-05-03 20:05:51 +0530

    card component

---
+++ src/card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card"> 
+        <img></img>
+        <h2>Rohith Krishna P</h2>
+        <p>Soft. Dev</p>
+        </div>
+    );
+}
+export default Card
