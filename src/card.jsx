3ec33d9df17fef2c91a403890e084f0dd902de89
Rohith Krishna <roal2027@gmail.com>
2024-05-31 11:18:01 +0530

    image source updated

--- src/card.jsx
+++ src/card.jsx
@@ -1,10 +1,10 @@
-function Card(){
-    return(
-        <div className="card"> 
-        <img src=""></img>
-        <h2>Rohith Krishna P</h2>
-        <p>Soft. Dev</p>
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://picsum.photos/200" alt="Profile" />
+            <h2>Rohith Krishna P</h2>
+            <p>Soft. Dev</p>
         </div>
     );
 }
-export default Card
+export default Card;
